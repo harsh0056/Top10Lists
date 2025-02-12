e5d9243605d4dae56aa612d1b10ee73e87c198e5
harsh0056 <98757427+harsh0056@users.noreply.github.com>
2025-02-12 03:28:43 +0000

    Assistant checkpoint: Update SingleBlog component to fetch data from blogData
    Assistant generated file changes:
    - src/pages/SingleBlog.jsx: Update SingleBlog component to handle blog data properly
    
    ---
    
    User prompt:
    
    Please update my singleBlog page right now when i am clicking on any blog it gives me blog not found, please fix that with the data coming from blogData

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,63 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { useLoaderData, useParams } from 'react-router-dom'
+import React from 'react'
+import { useParams } from 'react-router-dom'
 import { FaUser } from "react-icons/fa";
 import { FaClock } from 'react-icons/fa6';
 import Sidebar from '../components/Sidebar';
-import { useLocation } from 'react-router-dom';
-
-
+import blogData from '../assets/blogsData.json';
 
 const SingleBlog = () => {
-    
-    const { id } = useParams(); 
-    const location = useLocation();
-    
-    const { blog } = location.state || {};
+    const { id } = useParams();
+    const blog = blogData.find(blog => blog.id === parseInt(id));
 
     if (!blog) {
-      return (
-        <div className='py-36 bg-black text-white text-center px-4'>
-                <h1 className='text-5xl leading-snug font-bold mb-5'>NO Blog found for blog id : {id}</h1>
+        return (
+            <div className='py-36 bg-black text-white text-center px-4'>
+                <h1 className='text-5xl leading-snug font-bold mb-5'>Blog not found</h1>
             </div>
-      )
+        );
     }
 
-
-
-    console.log(data)
-    const { title, author, content, image, reading_time, published_date } = data[0];
-    console.log(title)
     return (
         <div>
             <div className='py-36 bg-black text-white text-center px-4'>
                 <h1 className='text-5xl leading-snug font-bold mb-5'>Single Blog</h1>
             </div>
 
-            <div className=' my-12 flex flex-col md:flex-row gap-12'>
+            <div className='max-w-7xl mx-auto my-12 flex flex-col md:flex-row gap-12'>
                 <div className='lg:w-3/4 mx-auto'>
-                    <div><img src={blog.image} alt="" className='mx-auto w-full rounded mb-5' /></div>
-                    <h2 className='text-3xl font-bold mb-4 text-blue-600 cursor-pointer'>{blog.title}</h2>
-                    <p className='mb-3 text-gray-600'><FaUser className='inline-flex items-center mr-2' /> {blog.author} | {blog.published_date}</p>
-                    <p className='mb-6 text-gray-600'><FaClock className='inline-flex items-center mr-2' /> {blog.reading_time}</p>
-                    <p className='text-sm text-gray-500 mb-6'>{blog.content} </p>
-                    <div className='text-base text-gray-500'>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis sapiente necessitatibus unde ullam porro veritatis id suscipit facilis accusantium deserunt quibusdam voluptatem magni provident iste, ex quisquam dignissimos nostrum non?
-                            <br /> <br />
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo laudantium totam tempora quas modi exercitationem earum aliquid cupiditate, iure corporis praesentium quis error itaque voluptatibus rem maiores! Quod, est voluptatibus.
-                            <br /> <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt at aliquid ea suscipit dolorum ipsum assumenda error. Magnam ratione obcaecati repellendus saepe, odit eveniet temporibus nostrum laborum, nulla aliquam officia, iusto nihil error. Reiciendis velit, tempore aliquid nobis nam sint veniam error quod ut accusantium tempora voluptate officiis neque omnis itaque, accusamus quo nisi repudiandae ab aperiam natus eligendi. Repellat optio facere atque sequi, exercitationem voluptas iusto et natus aspernatur aliquam, accusamus enim iste recusandae, molestias magnam aliquid deserunt quidem alias ullam odit! Autem, totam hic natus ab ullam aliquid quo vitae at illo cumque exercitationem saepe pariatur incidunt fugiat qui possimus cupiditate voluptatem id deserunt asperiores ad ut est! Aspernatur voluptates possimus minima in ipsa! Amet animi odit beatae velit? Recusandae velit aut impedit animi vero nostrum officiis aperiam eum reprehenderit iusto veritatis quia, sapiente nulla, consectetur distinctio voluptate quo qui aliquam deleniti voluptatibus ea laborum. Sapiente a dicta dolor magni Sed
-                        <br /> <br />
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae aut culpa repudiandae asperiores, placeat, alias veritatis ex minus consectetur tenetur libero fuga et eligendi! Distinctio, incidunt ipsa nisi eaque amet consequuntur neque voluptates perspiciatis. Laboriosam ducimus quia optio et. Libero possimus voluptas iusto rem nam nemo cumque consequatur nostrum ipsam? Laborum odio nostrum itaque ducimus molestias assumenda aut numquam blanditiis ratione praesentium nobis ut voluptate eaque iste provident sapiente eius excepturi at suscipit quam, quas possimus officiis quia aliquid! Recusandae.
-                        </p>
+                    <div>
+                        <img src={blog.image} alt="" className='w-full rounded mb-5' />
                     </div>
+                    <h2 className='text-3xl font-bold mb-4 text-blue-600 cursor-pointer'>{blog.title}</h2>
+                    <p className='mb-3 text-gray-600'>
+                        <FaUser className='inline-flex items-center mr-2' /> 
+                        {blog.author} | {blog.published_date}
+                    </p>
+                    <p className='mb-6 text-gray-600'>
+                        <FaClock className='inline-flex items-center mr-2' /> 
+                        {blog.reading_time}
+                    </p>
+                    <p className='text-base text-gray-500 mb-6'>{blog.content}</p>
                 </div>
-                <div className='lg:w-1/2'>
-                <Sidebar/>
+                <div className='lg:w-1/4'>
+                    <Sidebar />
                 </div>
-
             </div>
         </div>
-    )
+    );
 }
 
-export default SingleBlog
+export default SingleBlog;
