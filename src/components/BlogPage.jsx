dcd6be69bcd23fe0540d9e2138ef79233e076e02
harsh0056 <98757427+harsh0056@users.noreply.github.com>
2025-02-12 03:11:12 +0000

    Assistant checkpoint: Update layout to keep sidebar fixed on right
    Assistant generated file changes:
    - src/components/BlogPage.jsx: Update layout structure
    - src/components/BlogCards.jsx: Adjust blog cards grid
    
    ---
    
    User prompt:
    
    I want the Popular blog and latest blog section to always come on the right whatever be the size of the window , please apply this change

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -45,15 +45,21 @@ const BlogPage = () => {
       </div>
 
       {/* Blog Cards + Sidebar */}
-      <div className="flex flex-col lg:flex-row gap-12">
-        <BlogCards
-          blogs={blogsToShow}
-          currentPage={currentPage}
-          selectedCategory={selectedCategory}
-          pageSize={pageSize}
-        />
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className="lg:w-[75%] w-full">
+          <BlogCards
+            blogs={blogsToShow}
+            currentPage={currentPage}
+            selectedCategory={selectedCategory}
+            pageSize={pageSize}
+          />
+        </div>
         
-        <Sidebar />
+        <div className="lg:w-[25%] w-full">
+          <div className="lg:sticky lg:top-24">
+            <Sidebar />
+          </div>
+        </div>
       </div>
 
       <Pagination
